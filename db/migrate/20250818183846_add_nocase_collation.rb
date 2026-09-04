6f9ef19cf8dbac8bb9acb000d5898fd7fe56f554
usiegl00 <50933431+usiegl00@users.noreply.github.com>
2026-09-04 20:33:11 +0900

    Fix NOCASE migration.

--- db/migrate/20250818183846_add_nocase_collation.rb
+++ db/migrate/20250818183846_add_nocase_collation.rb
@@ -1,13 +1,13 @@
 class AddNocaseCollation < ActiveRecord::Migration[8.0]
   def up
     if connection.adapter_name == "PostgreSQL"
-      execute("CREATE COLLATION IF NOT EXISTS 'NONAME' (provider = icu, locale = 'und-u-ks-level2', deterministic = false);")
+      execute("CREATE COLLATION IF NOT EXISTS \"NOCASE\" (provider = icu, locale = 'und-u-ks-level2', deterministic = false);")
     end
   end
 
   def down
     if connection.adapter_name == "PostgreSQL"
-      execute("DROP COLLATION IF EXISTS 'NONAME';")
+      execute("DROP COLLATION IF EXISTS \"NOCASE\";")
     end
   end
 end
