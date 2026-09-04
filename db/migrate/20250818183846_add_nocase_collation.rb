06dcc9489e84beec719605bdf0ddd052d86bc54f
usiegl00 <50933431+usiegl00@users.noreply.github.com>
2026-09-04 20:24:09 +0900

    Fix migration name.

--- db/migrate/20250818183846_add_nocase_collation.rb
+++ db/migrate/20250818183846_add_nocase_collation.rb
@@ -1,4 +1,4 @@
-class AddNocaseCollationToUsername < ActiveRecord::Migration[8.0]
+class AddNocaseCollation < ActiveRecord::Migration[8.0]
   def up
     if connection.adapter_name == "PostgreSQL"
       execute("CREATE COLLATION IF NOT EXISTS 'NONAME' (provider = icu, locale = 'und-u-ks-level2', deterministic = false);")
